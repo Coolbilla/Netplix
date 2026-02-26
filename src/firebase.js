bda721bc03f76706cc4cb51b3c90004125a5ccfd
Coolbilla <ishaangarg0904@gmail.com>
2026-02-27 00:17:39 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 // Example: authDomain: "www.my-movie-site.com"
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  authDomain:"netplayer2-eba6b.firebaseapp.com",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
