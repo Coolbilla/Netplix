ea168908dc70a65ad67fd7da829fd5a0b5bcffb6
Coolbilla <ishaangarg0904@gmail.com>
2026-03-02 15:25:53 +0530

    Update Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -53,7 +53,7 @@ const Player = ({ media, toggleCompleted, onClose }) => {
     // SERVER URLS
     const cinezoUrl = getFlixerUrl(tmdbId, type, season, episode);
     const vidoraUrl = type === 'tv' ? `https://vidora.me/tv/${tmdbId}/${season}/${episode}?autoplay=true&colour=00ffff` : `https://vidora.su/movie/${tmdbId}?autoplay=true&colour=00ffff`;
-    const vidsrcUrl = type === 'tv' ? `https://vidsrc.me/embed/${tmdbId}/${season}-${episode}` : `https://vidsrc.me/embed/${tmdbId}`;
+    const vidsrcUrl = type === 'tv' ? `https://vidsrc.su/embed/${tmdbId}/${season}-${episode}` : `https://vidsrc.su/embed/${tmdbId}`;
     const twoEmbedUrl = type === 'tv' ? `https://111movies.net/tv/${tmdbId}/${season}/${episode}` : `https://111movies.net/movie/${tmdbId}`;
 
     let streamUrl = cinezoUrl;
